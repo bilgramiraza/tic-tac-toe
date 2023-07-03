0afe88bcdb3237dabcf9f7a3dcae6e0dc3885c9c
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-04 01:05:30 +0530

    Pulled State up to board level for game state management

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import { useState } from 'react';
 import './App.css';
 
-function Square({ input }){
-  const [value, setValue] = useState('');
-  function handleClick(){
-    setValue(input);
-  }
-  return <button className='square' onClick={handleClick}>{ value }</button>
+function Square({ input, onSquareClick }){
+  return <button className='square' onClick={onSquareClick}>{ input }</button>
 }
 
 function Board(){
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(index){
+    const newSquares = squares.slice();
+    newSquares[index] = 'X';
+    setSquares(newSquares);
+  }
   return <>
       <div className='board-row'>
-        <Square input='1'/>
-        <Square input='2'/>
-        <Square input='3'/>
+        <Square input={squares[0]} onSquareClick={()=>{handleClick(0)}}/>
+        <Square input={squares[1]} onSquareClick={()=>{handleClick(1)}}/>
+        <Square input={squares[2]} onSquareClick={()=>{handleClick(2)}}/>
       </div>
       <div className='board-row'>
-        <Square input='4'/>
-        <Square input='5'/>
-        <Square input='6'/>
+        <Square input={squares[3]} onSquareClick={()=>{handleClick(3)}}/>
+        <Square input={squares[4]} onSquareClick={()=>{handleClick(4)}}/>
+        <Square input={squares[5]} onSquareClick={()=>{handleClick(5)}}/>
       </div>
       <div className='board-row'>
-        <Square input='7'/>
-        <Square input='8'/>
-        <Square input='9'/>
+        <Square input={squares[6]} onSquareClick={()=>{handleClick(6)}}/>
+        <Square input={squares[7]} onSquareClick={()=>{handleClick(7)}}/>
+        <Square input={squares[8]} onSquareClick={()=>{handleClick(8)}}/>
       </div>
     </>;
 }
