26082e9bca315621daeeaddc766d500e0a815058
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-04 05:46:26 +0530

    Cleaned up App Return and Modified it to show current move as text

--- src/App.js
+++ src/App.js
@@ -78,13 +78,13 @@ function Game (){
   }
   const moves = history.map((_squares,move)=>{
     let desc;
-    
-    if(move>0) desc=`Go to Move ${move}`;
-    else desc='Go to game Start';
+    if(move===history.length-1) desc = `You are at Move ${move+1}`;
+    else if(move>0) desc = (<button onClick={()=> jumpTo(move)}>{`Go to Move ${move}`}</button>);
+    else desc = (<button onClick={()=> jumpTo(move)}>Go to Game Start</button>);
 
     return (
       <li key={move}>
-        <button onClick={()=> jumpTo(move)}>{desc}</button>
+        {desc}  
       </li>
     );
   });
@@ -102,9 +102,7 @@ function Game (){
 
 function App() {
   return (
-    <>
     <Game/>
-    </>
   );
 }
 
