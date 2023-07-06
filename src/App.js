01ff4c90e7b3f0b6360729585b2ac41e57309ecf
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-06 07:36:35 +0530

    Added Ability to set Board Size using a form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
+import Form from "./components/Form";
 import Game from "./components/Game";
 
 const App = () => {
   //TODO:Setup Home Screen to select Board size and ability to Restart matches
-  //Cleanup-TODO: Seperate consts to different modules for maintainability
   //Later TODO: Look into using trees to store match history
   //GIGA-TODO: AI Bot?
-  const size = 4;
-  return (<Game size={size}/>);
+  const [size, setSize] = useState();
+
+  return (<div>
+    {size?<Game size={size}/>:<Form setSize={setSize}/>}
+  </div>);
 }
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const Form = ({ setSize }) => {
+  const [sizeInput, setSizeInput] = useState(3);
+
+  const handleSizeInput = (e) =>{
+    setSizeInput(parseInt(e.target.value));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSize(sizeInput);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} >
+      <input type="number" 
+        id="boardSize" 
+        name="boardSize" 
+        onChange={handleSizeInput} 
+        value={sizeInput} 
+        min={3}
+        defaultValue={3}
+      />
+      <button>Set Size</button>
+    </form>
+  );
+};
+
+export default Form;
