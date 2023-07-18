2fc9f49a0e8bf2f28fc96d1e3fc65ff0b39cff02
bilgramiraza <bilgramiraza@yahoo.com>
2023-07-18 10:48:33 +0530

    Removed Todo Comments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Form from "./components/Form";
 import Game from "./components/Game";
 
 const App = () => {
-  //TODO:Setup Home Screen to select Board size and ability to Restart matches
   const [size, setSize] = useState(null);
 
   return (<div>
